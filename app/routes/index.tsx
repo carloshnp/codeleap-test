fd55b56a8c50e2eb08d84454314dcb2bfcaf0bb9
carloshnp <carloshnpwork@gmail.com>
2023-05-16 23:10:55 -0300

    feat: sign-up form

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,19 @@ import View from "~/components/View";
 export default function Index() {
   return (
     <View>
-      This is an app.
+      <div className="w-128 h-52 p-6 bg-white rounded-2xl">
+        <h1>Welcome to CodeLeap network!</h1>
+        <form className="flex flex-col">
+          <label>Please enter your username</label>
+          <input
+            type="text"
+            id="username"
+          />
+          <div className="flex justify-end">
+            <button className="w-28 h-8 bg-[#7695EC] text-white rounded-lg">ENTER</button>
+          </div>
+        </form>
+      </div>
     </View>
   )
 }
